b0b5b18e1d553d39cdeee82c7216d4daf7827fca
Kai Chang <kai.chang@morningstar.com>
2016-05-01 14:25:30 -0500

    Add buildUri helper function
    Add helper function that takes a number of string paths and builds a Uri.

--- app/src/main/java/com/example/kai/locallore/database/LoreProvider.java
+++ app/src/main/java/com/example/kai/locallore/database/LoreProvider.java
@@ -1,5 +1,6 @@
 package com.example.kai.locallore.database;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 
 import net.simonvt.schematic.annotation.ContentProvider;
@@ -12,18 +13,27 @@ import net.simonvt.schematic.annotation.TableEndpoint;
 @ContentProvider(authority = LoreProvider.AUTHORITY, database = LoreDatabase.class)
 public class LoreProvider {
 
-    public static final String AUTHORITY = "com.example.kai.locallore.LoreProvider";
+    public static final String AUTHORITY = "com.example.kai.locallore";
     static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
 
     interface Path {
         String LORE = "lore";
     }
 
+    private static Uri buildUri(String... paths) {
+        Uri.Builder builder = BASE_CONTENT_URI.buildUpon();
+
+        for (String path : paths) {
+            builder.appendPath(path);
+        }
+        return builder.build();
+    }
+
     @TableEndpoint(table = LoreDatabase.LORE)
     public static class Lore {
         @ContentUri(
-                path = "lore",
-                type = "locallore/lore",
+                path = Path.LORE,
+                type = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + Path.LORE,
                 defaultSort = LoreColumns.TITLE + " ASC")
         public static final Uri LORE = Uri.parse("content://" + AUTHORITY + "lore");
     }
