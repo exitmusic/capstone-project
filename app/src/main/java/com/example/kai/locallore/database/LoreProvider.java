81db19b574773bb832f3c428616ff8690926e4a0
Kai Chang <kai.chang@morningstar.com>
2016-05-01 13:54:07 -0500

    Add table endpoint to content provider
    Add a sample table endpoint to return a list of stories based on the example from the schematic readme.

--- app/src/main/java/com/example/kai/locallore/database/LoreProvider.java
+++ app/src/main/java/com/example/kai/locallore/database/LoreProvider.java
@@ -1,7 +1,25 @@
 package com.example.kai.locallore.database;
 
+import android.net.Uri;
+
+import net.simonvt.schematic.annotation.ContentProvider;
+import net.simonvt.schematic.annotation.ContentUri;
+import net.simonvt.schematic.annotation.TableEndpoint;
+
 /**
  * Created by kchang on 5/1/16.
  */
+@ContentProvider(authority = LoreProvider.AUTHORITY, database = LoreDatabase.class)
 public class LoreProvider {
+
+    public static final String AUTHORITY = "com.example.kai.locallore.LoreProvider";
+
+    @TableEndpoint(table = LoreDatabase.LORE)
+    public static class Lore {
+        @ContentUri(
+                path = "lore",
+                type = "locallore/lore",
+                defaultSort = LoreColumns.TITLE + " ASC")
+        public static final Uri LORE = Uri.parse("content://" + AUTHORITY + "lore");
+    }
 }
