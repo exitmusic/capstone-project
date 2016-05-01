08dfc4f893ae60f96b65f95430924b0f0618ceb0
Kai Chang <kai.chang@morningstar.com>
2016-05-01 13:11:46 -0500

    Create database
    Create a database that uses the LoreColumns interface class.

--- app/src/main/java/com/example/kai/locallore/database/LoreDatabase.java
+++ app/src/main/java/com/example/kai/locallore/database/LoreDatabase.java
@@ -1,7 +1,15 @@
 package com.example.kai.locallore.database;
 
+import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.Table;
+
 /**
  * Created by kchang on 5/1/16.
  */
+@Database(version = LoreDatabase.VERSION)
 public class LoreDatabase {
+
+    public static final int VERSION = 1;
+
+    @Table(LoreDatabase.class) public static final String LORE = "lore";
 }
