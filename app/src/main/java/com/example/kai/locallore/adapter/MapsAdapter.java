eb53a070bc7b7c650e13f7bdd02e8a51b832c2ce
Kai Chang <kai.chang@morningstar.com>
2016-05-03 22:40:00 -0500

    Extend MapsAdapter from CursorAdapter

--- app/src/main/java/com/example/kai/locallore/adapter/MapsAdapter.java
+++ app/src/main/java/com/example/kai/locallore/adapter/MapsAdapter.java
@@ -1,7 +1,27 @@
 package com.example.kai.locallore.adapter;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
 /**
  * Created by kchang on 5/3/16.
  */
-public class MapsAdapter {
+public class MapsAdapter extends CursorAdapter {
+    
+    public MapsAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
 }
