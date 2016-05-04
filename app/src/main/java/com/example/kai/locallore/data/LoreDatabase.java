4dc3376897e2a170e4c6f8401ece16081be29e7c
Kai Chang <kai.chang@morningstar.com>
2016-05-04 18:38:32 -0500

    Add database methods
    Add onCreate, onUpgrade, and onConfigure db methods. Also add @ExecOnCreate annotation

--- app/src/main/java/com/example/kai/locallore/data/LoreDatabase.java
+++ app/src/main/java/com/example/kai/locallore/data/LoreDatabase.java
@@ -1,6 +1,13 @@
 package com.example.kai.locallore.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.ExecOnCreate;
+import net.simonvt.schematic.annotation.OnConfigure;
+import net.simonvt.schematic.annotation.OnCreate;
+import net.simonvt.schematic.annotation.OnUpgrade;
 import net.simonvt.schematic.annotation.Table;
 
 /**
@@ -11,5 +18,25 @@ public class LoreDatabase {
 
     public static final int VERSION = 1;
 
-    @Table(LoreDatabase.class) public static final String LORE = "lore";
+    @Table(LoreDatabase.class)
+    public static final String LORE = "lore";
+
+    @OnCreate
+    public static void onCreate(Context context, SQLiteDatabase db) {
+
+    }
+
+    @OnUpgrade
+    public static void onUpgrade(Context context, SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    @OnConfigure
+    public static void onConfigure(SQLiteDatabase db) {
+
+    }
+
+    @ExecOnCreate
+    public static final String EXEC_ON_CREATE = "SELECT * FROM " + LORE;
+
 }
