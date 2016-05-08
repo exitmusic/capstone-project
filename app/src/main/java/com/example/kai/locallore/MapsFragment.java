d7396bdc0549bdaee70543227632ff31f4b44173
Kai Chang <kai.chang@morningstar.com>
2016-05-08 16:46:04 -0500

    Refactor activity_maps layout to fragment_maps

--- app/src/main/java/com/example/kai/locallore/MapsFragment.java
+++ app/src/main/java/com/example/kai/locallore/MapsFragment.java
@@ -58,7 +58,7 @@ public class MapsFragment extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.fragment_maps);
         ButterKnife.bind(this);
         mPackageManager = getApplicationContext().getPackageManager();
 
